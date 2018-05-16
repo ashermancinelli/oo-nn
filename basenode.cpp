e25bb790363a096870a8eb2a9dee3fa3874c7cf6
ashermancinelli <ashermancinelli@gmail.com>
2018-05-16 13:18:29 -0700

    progress on basenode.cpp

--- basenode.cpp
+++ basenode.cpp
@@ -1,14 +1,20 @@
 
 
 #include "basenode.h"
+#include "baselink.h"
+#include "llink.h"
 
 Base_Node(int v_size = 1, int e_size = 1);
 
 ~Base_Node(void);
 
-LList *In_Links(void);
+LList *In_Links(void) {
+	return &in_links;
+};
 
-LList *Out_Links(void);
+LList *Out_Links(void) {
+	return &out_links;
+};
 
 void Base_Node::Run(int mode=0)
 
@@ -41,4 +47,39 @@ void Base_Node::Create_Link_To(Base_Node &to_node, Base_Link *link) {
 
 void Base_Node::Print(std::ofstream &out)
 
+void Connect(Base_Node &from_node, Base_Node &to_node, Base_Link *link) {
+	from_node.Create_Link_To(to_node, link);
+}
+
+void Connect(Base_Node &from_node, Base_Node &to_node, Base_Link &link) {
+	from_node.Create_Link_To(to_node, &link);
+}
+
+void Connect(Base_Node *from_node, Base_Node *to_node, Base_Link *link) {
+	from_node.Create_Link_To(*to_node, link);
+}
+
+int Disconnect(Base_Node *from_node, Base_Node *to_node) {
+	LList *out_links = from_node->Out_Links();
+	int flag = 0;	// Flag is set if the link is found
+	out_links->Reset_To_Head();
+	for (int i = 0; i < out_links->Count(); i++) {
+		if (out_links->Curr()->Out_Node() == to_node) {
+			flag = 1;
+			break;	
+		}	
+		out_links->Next();
+	}
+
+	if (flag == 1) {	
+		out_links->Curr()->Out_Node()->In_Links->Del(out_links->Curr());	
+		out_links->Del_Node();
+		return 1;
+	} else {
+		return 0;	// link not found 
+	}
+
+};
+
+double Random(double lower_bound, dobule upper_bound);
 
