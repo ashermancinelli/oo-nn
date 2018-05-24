fb0f2efdebbd5e8420d31e0ba95121e326250539
Mancinelli, Asher J <asher.mancinelli@pnnl.gov>
2018-05-24 08:30:15 -0700

    updated backprop.h

--- lib/backprop.h
+++ lib/backprop.h
@@ -106,9 +106,89 @@ double BP_Output_Node::Compute_Error(int mode);
 	return value[NODE_VALUE] * (1.0 - value[NODE_VALUE]) * (error[NODE_ERROR] - value[NODE_VALUE]);
 };
 
+void BP_Output_Node::Learn(int mode)
+{
+	double delta;
+	error[NODE_ERROR] = Compute_Error();	
+	in_links.Reset_To_Head();
+	Base_Link *link;
+	int cnt = in_links.Count();
+	
+	fori(cnt)
+	{
+		link = in_links.Curr();
+		delta = value[LEARNING_RATE] * error[NODE_ERROR] * link->In_Value(); // delta rule
+		link->Update_Weight(delta);
+		in_links.Next();
+	}
 
+};
 
+char *BP_Output_Node::Get_Name(void)
+{
+	static char name[] = "BP_OUTPUT_NODE";
+	return name;
+};
 
+BP_Middle_Node::BP_Middle_Node(double lr, double mt, int v_size, int e_size) : BP_Output_Node(lr, mt, v_size, e_size) {};
+
+char *BP_Middle_Node::Get_Name(void)
+{
+	static char name[] = "BP_MIDDLE_NODE";
+	return name;
+};
+
+double BP_Middle_Node::Compute_Error(int mode)
+{
+	double total = 0;
+	out_links.Reset_To_Head();
+	int cnt = out_links.Count();
+	fori(cnt)
+	{
+		total += out_links.Curr()->Weighted_Out_Error();
+		out_links.Next();
+	}
+	return value[NODE_VALUE] * ( 1.0 - value[NODE_VALUE]) * total;
+};
+
+char *BP_Middle_Node::Get_Name(void)
+{
+	static char name[] = "BP_MIDDLE_NODE";
+	return name;
+};
+
+class Backprop_Network : public ADALINE_Network
+{
+protected:
+	int num_layers;
+	int first_middle_node;
+	int first_output_node;
+	int *node_cnt;
+	double momentum_term;
+	virtual void Create_Network(void);
+
+public:
+	Backprop_Network(char filename[]);
+	Backprop_Network(double lr, double mt, int layers, ...);
+	Backprop_Network(double lr, double mt, int layers, int nodes[]);
+	Backprop_Network(void);
+	~Backprop_Network(void);
+	virtual	double Get_Error(int id = 0);
+	virtual void Set_Error(double new_val, int id = 0);
+	virtual void Set_Error(Pattern *input_pattern);
+	virtual double Get_Value(int id = 0);
+	virtual void Save(std::ofstream &outfile);
+	virtual void Load(std::ifstream &infile);
+	virtual void Run(int mode = 0);
+	virtual void Learn(int mode = 0);
+	virtual char *Get_Name(void);
+	virtual int Get_Num_Layers(void);
+	virtual int Get_Layer_Count(int id);
+};
+
+void Backprop_Network::Create_Network(void)
+{
+	
 
 
 
