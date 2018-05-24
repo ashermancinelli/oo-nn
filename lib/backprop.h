b52330ca563253fac337dd79a49c8122c638e054
Mancinelli, Asher J <asher.mancinelli@pnnl.gov>
2018-05-23 22:15:27 -0700

    secondary backprop.h progress

--- lib/backprop.h
+++ lib/backprop.h
@@ -30,6 +30,49 @@ public:
 	virtual void Update_Weight(double new_val);
 };
 
+class BP_Output_Node : public BP_Node
+{
+public:
+	BP_Output_Node(double lr, double mt, int v_size=3, int e_size=1);
+	// default value set is: NODE_VALUE, LEARNING_RATE, MOMENTUM
+	// default error set is: NODE_ERROR
+protected:
+	virtual double Compute_Error(int mode=0);
+	virtual void Learn(int mode=0);
+	virtual char *Get_Name(void);
+};
+
+class BP_Middle_Node : public BP_Output_Node
+{
+public:
+	BP_Middle_Node(double lr, double mt, int v_size=3, int e_size=1);
+	// Same defuatl values as BP_Output_Node
+	virtual char *Get_Name(void);
+protected:
+	virtual double Compute_Error(int mode=0);
+};
+
+double BP_Node::Transfer_Function(double value)
+{
+	return 1.0/(1.0 + exp(-value)); // Sigmoid function
+};
+
+BP_Node::BP_Node(int v_size, int e_size) : Feed_Forward:Node(v_size, e_size) {};
+
+char *BP_Node::Get_Name(void)
+{
+	static char name[] = "BP_NODE";
+	return name;
+};
+
+BP_Link::BP_Link(int size)
+{
+	value[WEIGHT] = Random(-1.0, 1.0);
+	value[DELTA] = 0.0;
+};
+
+
+
 
 
 
